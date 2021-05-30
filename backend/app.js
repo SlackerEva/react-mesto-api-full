a2d1542afa2c3bcf3a91f73580121e08a1138f57
SlackerEva <lesiv_anna@mail.ru>
2021-05-30 18:26:46 +0100

    fix: change key in auth

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const { middleError } = require('./middlewares/middleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 dotenv.config();
+
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 const app = express();
 
